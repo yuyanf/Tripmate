e519851229795db7cb137db2d6def2b790f7becf
Anton Vo <antonlevo0903@gmail.com>
2024-02-10 15:35:17 +0100

    adjust header style

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,7 +4,7 @@ import { Button } from "./ui/button";
 
 const Header = () => {
   return (
-    <header className="fixed inset-x-0 top-0 z-50 h-20 px-6 backdrop-blur-md">
+    <header className=" fixed inset-x-0 top-0 z-50 h-20 w-full border-b border-stone-100 bg-white/[0.6] px-6 backdrop-blur-sm dark:border-white/[0.1] dark:bg-black/[0.6]">
       <div className="mx-auto flex h-full w-full max-w-7xl items-center justify-between">
         <Link href="/">
           <Logo className="h-full w-20 sm:w-40" />
