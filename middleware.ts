cf8c2ae9752829659d941a7a61585c4dc325865b
Anton Vo <antonlevo0903@gmail.com>
2024-02-14 23:09:58 +0100

    adjust middleware to redirect unauthed user back to home and allow authed users to access private routes

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,23 @@
-import { authMiddleware } from "@clerk/nextjs/server";
-
+import { authMiddleware } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
-  publicRoutes: ["/"],
+  afterAuth(auth, req, evt) {
+    // Handle users who aren't authenticated
+    if (!auth.userId && !auth.isPublicRoute) {
+      return NextResponse.redirect("/");
+    }
+
+    // If the user is logged in and trying to access a protected route, allow them to access route
+    if (auth.userId && !auth.isPublicRoute) {
+      return NextResponse.next();
+    }
+
+    // Allow users visiting public routes to access them
+    return NextResponse.next();
+  },
 });
 
 export const config = {
