245fd907252f8690a6293071143770d83e36112a
Jenny Fei <yuyans.are@gmail.com>
2024-02-19 21:26:55 +0100

    revert middlware for test

--- middleware.ts
+++ middleware.ts
@@ -4,19 +4,19 @@ import { NextResponse } from "next/server";
 export default authMiddleware({
   publicRoutes: ["/"],
 
-  afterAuth(auth, req, evt) {
-    // Handle users who aren't authenticated
-    if (!auth.userId && !auth.isPublicRoute) {
-      return NextResponse.redirect(req.nextUrl.origin);
-    }
+  // afterAuth(auth, req, evt) {
+  //   // Handle users who aren't authenticated
+  //   if (!auth.userId && !auth.isPublicRoute) {
+  //     return NextResponse.redirect(req.nextUrl.origin);
+  //   }
 
-    // If the user is logged in and trying to access a protected route, allow them to access route
-    if (auth.userId && !auth.isPublicRoute) {
-      return NextResponse.next();
-    }
-    // Allow users visiting public routes to access them
-    return NextResponse.next();
-  },
+  //   // If the user is logged in and trying to access a protected route, allow them to access route
+  //   if (auth.userId && !auth.isPublicRoute) {
+  //     return NextResponse.next();
+  //   }
+  //   // Allow users visiting public routes to access them
+  //   return NextResponse.next();
+  // },
 });
 
 export const config = {
